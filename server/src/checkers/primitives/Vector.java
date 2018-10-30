92c364be6d8aa461e9e20dc4e64153a8a73c4c16
Roman Khapov <r.khapov@yandex.ru>
2018-10-30 23:53:18 +0500

    3d for vector

--- server/src/checkers/primitives/Vector.java
+++ server/src/checkers/primitives/Vector.java
@@ -6,27 +6,31 @@ public class Vector {
 
   private final int x;
   private final int y;
+  private final int z;
 
-  public static final Vector zero = new Vector(0, 0);
-  public static final Vector xUnit = new Vector(0, 1);
-  public static final Vector yUnit = new Vector(1, 0);
+  public static final Vector zero = new Vector(0, 0, 0);
+  public static final Vector xUnit = new Vector(1, 0, 0);
+  public static final Vector yUnit = new Vector(0, 1, 0);
+  public static final Vector zUnit = new Vector(0, 0, 1);
 
   private static final Random random = new Random();
 
-  public Vector(int y, int x) {
+  public Vector(int x, int y, int z) {
     this.x = x;
     this.y = y;
+    this.z = z;
   }
 
-  public static Vector create(int y, int x) {
-    return new Vector(y, x);
+  public static Vector create(int x, int y, int z) {
+    return new Vector(x, y, z);
   }
 
-  public static Vector createRandom(int minY, int maxY, int minX, int maxX) {
+  public static Vector createRandom(int minX, int maxX, int minY, int maxY, int minZ, int maxZ) {
     var y = random.nextInt(Math.abs(maxY - minY)) + minY;
     var x = random.nextInt(Math.abs(maxX - minX)) + minX;
+    var z = random.nextInt(Math.abs(maxZ - minZ)) + minZ;
 
-    return new Vector(y, x);
+    return new Vector(x, y, z);
   }
 
   public int getX() {
@@ -35,6 +39,10 @@ public class Vector {
 
   public int getY() {
     return y;
+  }
+
+  public int getZ() {
+    return z;
   }
 
   @Override
@@ -49,7 +57,7 @@ public class Vector {
 
     if (obj instanceof Vector) {
       var other = (Vector) obj;
-      return x == other.x && y == other.y;
+      return x == other.x && y == other.y && z == other.z;
     }
 
     return false;
@@ -57,38 +65,40 @@ public class Vector {
 
   @Override
   public int hashCode() {
-    return (x * 1033) ^ y;
+    return ((x * 1033) ^ y * 1033) ^ z;
   }
 
   @Override
   public String toString() {
-    return String.format("(%d;%d)", y, x);
+    return String.format("(%d;%d;%d)", x, y, z);
   }
 
   public Vector add(Vector vector) {
-    return new Vector(y + vector.y, x + vector.x);
+    return new Vector(x + vector.x, y + vector.y, z + vector.z);
   }
 
   public Vector sub(Vector vector) {
-    return new Vector(y - vector.y, x - vector.x);
+    return new Vector(x - vector.x, y - vector.y, z - vector.z);
   }
 
   public Vector mul(int k) {
-    return new Vector(y * k, x * k);
+    return new Vector(x * k, y * k, z * k);
   }
 
   public int getManhattanLength() {
     var dx = Math.abs(x);
     var dy = Math.abs(y);
+    var dz = Math.abs(z);
 
-    return dx + dy;
+    return dx + dy + dz;
   }
 
   public int getChebushevLength() {
     var dx = Math.abs(x);
     var dy = Math.abs(y);
+    var dz = Math.abs(z);
 
-    return Math.max(dy, dx);
+    return Math.max(Math.max(dy, dx), dz);
   }
 
   public static int getManhattanLength(Vector v1, Vector v2) {
--- server/src/tests/VectorUnitTests.java
+++ server/src/tests/VectorUnitTests.java
@@ -13,7 +13,7 @@ public class VectorUnitTests {
 
   @Test
   public void equals_comparingToNull_shouldReturnFalse() {
-    var v1 = new Vector(1, 2);
+    var v1 = new Vector(1, 2, 0);
 
     var sut = v1.equals(null);
 
@@ -22,7 +22,7 @@ public class VectorUnitTests {
 
   @Test
   public void equals_comparingToNonVectorType_shouldReturnFalse() {
-    var v = new Vector(1, 2);
+    var v = new Vector(1, 2, 0);
 
     var sut = v.equals(5);
 
@@ -31,7 +31,7 @@ public class VectorUnitTests {
 
   @Test
   public void equals_comparingToItSelf_shouldReturnTrue() {
-    var v = new Vector(1, 2);
+    var v = new Vector(1, 2, 0);
 
     var sut = v.equals(v);
 
@@ -40,8 +40,8 @@ public class VectorUnitTests {
 
   @Test
   public void equals_comparingToDifferentVectorInX_shouldReturnFalse() {
-    var v1 = new Vector(5, -1);
-    var v2 = new Vector(5, 1);
+    var v1 = new Vector(-1, 1, 9);
+    var v2 = new Vector(1, 1, 9);
 
     var sut = v1.equals(v2);
 
@@ -50,8 +50,18 @@ public class VectorUnitTests {
 
   @Test
   public void equals_comparingToDifferentVectorInY_shouldReturnFalse() {
-    var v1 = new Vector(-1, 6);
-    var v2 = new Vector(1, 6);
+    var v1 = new Vector(1, 6, 7);
+    var v2 = new Vector(1, -6, 7);
+
+    var sut = v1.equals(v2);
+
+    assertFalse(sut);
+  }
+
+  @Test
+  public void equals_comparingToDifferentVectorInZ_shouldReturnFalse() {
+    var v1 = new Vector(1, 6, 7);
+    var v2 = new Vector(1, 6, 8);
 
     var sut = v1.equals(v2);
 
@@ -60,8 +70,8 @@ public class VectorUnitTests {
 
   @Test
   public void equals_comparingToSameVector_shouldReturnTrue() {
-    var v1 = new Vector(1, 2);
-    var v2 = new Vector(1, 2);
+    var v1 = new Vector(1, 2, 3);
+    var v2 = new Vector(1, 2, 3);
 
     var sut = v1.equals(v2);
 
@@ -71,9 +81,9 @@ public class VectorUnitTests {
 
   @Test
   public void create_shouldReturnRightVector() {
-    var expected = new Vector(1, 2);
+    var expected = new Vector(1, 2, 3);
 
-    var sut = Vector.create(1, 2);
+    var sut = Vector.create(1, 2, 3);
 
     assertEquals(expected, sut);
   }
@@ -86,8 +96,8 @@ public class VectorUnitTests {
     var count = 0;
 
     for (var i = 0; i < 10000; i++) {
-      var v1 = Vector.createRandom(-1000, 1000, -1000, 1000);
-      var v2 = Vector.createRandom(-1000, 1000, -1000, 1000);
+      var v1 = Vector.createRandom(-1000, 1000, -1000, 1000, -1000, 1000);
+      var v2 = Vector.createRandom(-1000, 1000, -1000, 1000, -1000, 1000);
 
       if (v1.hashCode() == v2.hashCode() && !v1.equals(v2)) {
         count++;
@@ -99,44 +109,58 @@ public class VectorUnitTests {
 
   @Test
   public void zero_shouldReturnZeroVector() {
-    var expected = new Vector(0, 0);
+    var expected = new Vector(0, 0, 0);
 
     assertEquals(Vector.zero, expected);
   }
 
   @Test
   public void xUnit_shouldBeRightVector() {
-    var expected = new Vector(0, 1);
+    var expected = new Vector(1, 0, 0);
 
     assertEquals(Vector.xUnit, expected);
   }
 
   @Test
   public void yUnit_shouldBeRightVector() {
-    var expected = new Vector(1, 0);
+    var expected = new Vector(0, 1, 0);
 
     assertEquals(Vector.yUnit, expected);
   }
 
   @Test
+  public void zUnit_shouldBeRightVector() {
+    var expected = new Vector(0, 0, 1);
+
+    assertEquals(expected, Vector.zUnit);
+  }
+
+  @Test
   public void getX_shouldReturnRightValue() {
-    var v = Vector.create(0, -1337);
+    var v = Vector.create(-1337, -1337, 0);
 
     assertEquals(-1337, v.getX());
   }
 
   @Test
   public void getY_shouldReturnRightValue() {
-    var v = Vector.create(1337, 0);
+    var v = Vector.create(1337, -56, 123);
 
-    assertEquals(1337, v.getY());
+    assertEquals(-56, v.getY());
+  }
+
+  @Test
+  public void getZ_shouldReturnRightValue() {
+    var v = new Vector(133, -56, 123);
+
+    assertEquals(123, v.getZ());
   }
 
   @Test
   public void add_shouldReturnRightVector() {
-    var v1 = new Vector(1, 5);
-    var v2 = new Vector(-6, 10);
-    var expected = new Vector(-5, 15);
+    var v1 = new Vector(1, 5, 9);
+    var v2 = new Vector(-6, 10, 1);
+    var expected = new Vector(-5, 15, 10);
 
     var sut = v1.add(v2);
 
@@ -145,9 +169,9 @@ public class VectorUnitTests {
 
   @Test
   public void sub_shouldReturnRightValue() {
-    var v1 = new Vector(1, 5);
-    var v2 = new Vector(7, 9);
-    var expected = new Vector(-6, -4);
+    var v1 = new Vector(1, 5, 6);
+    var v2 = new Vector(7, 9, 3);
+    var expected = new Vector(-6, -4, 3);
 
     var sut = v1.sub(v2);
 
@@ -156,9 +180,9 @@ public class VectorUnitTests {
 
   @Test
   public void mul_shouldReturnRightValue() {
-    var v1 = new Vector(5, -7);
+    var v1 = new Vector(5, -7, 8);
     var k = -4;
-    var expected = new Vector(5 * -4, -7 * -4);
+    var expected = new Vector(5 * -4, -7 * -4, 8 * -4);
 
     var sut = v1.mul(k);
 
@@ -167,16 +191,16 @@ public class VectorUnitTests {
 
   @Test
   public void getManhattanLength_shouldReturnSumOfAbs() {
-    var v = new Vector(5, -7);
+    var v = new Vector(5, -7, 2);
 
     var sut = v.getManhattanLength();
 
-    assertEquals(5 + 7, sut);
+    assertEquals(5 + 7 + 2, sut);
   }
 
   @Test
   public void getChebushevLength_shouldReturnMaxOfAbs() {
-    var v = new Vector(7, -9);
+    var v = new Vector(7, -9, 3);
 
     var sut = v.getChebushevLength();
 
@@ -185,9 +209,9 @@ public class VectorUnitTests {
 
   @Test
   public void getManhattanLength_shouldReturnRightValue() {
-    var v1 = new Vector(5, -13);
-    var v2 = new Vector(10, 17);
-    var expected = 5 + 13 + 17;
+    var v1 = new Vector(5, -13, 5);
+    var v2 = new Vector(10, 17, 1);
+    var expected = 5 + 13 + 17 + 4;
 
     var sut1 = Vector.getManhattanLength(v1, v2);
     var sut2 = Vector.getManhattanLength(v2, v1);
@@ -198,8 +222,8 @@ public class VectorUnitTests {
 
   @Test
   public void getChebushevLength_shouldReturnRightValue() {
-    var v1 = new Vector(5, -13);
-    var v2 = new Vector(10, 17);
+    var v1 = new Vector(5, -13, 5);
+    var v2 = new Vector(10, 17, 1);
     var expected = 13 + 17;
 
     var sut1 = Vector.getChebushevLength(v1, v2);
