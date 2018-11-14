d9fcfc790fef3e37ddad5a7d73345e415c2d74cb
Roman Khapov <r.khapov@yandex.ru>
2018-11-14 10:44:56 +0500

    fix VectorException

--- server/src/core/checkers/primitives/VectorException.java
+++ server/src/core/checkers/primitives/VectorException.java
@@ -1,6 +1,8 @@
 package core.checkers.primitives;
 
-public class VectorException extends Exception {
+import core.tools.CoreException;
+
+public class VectorException extends CoreException {
   public VectorException(String msg) {
     super(msg);
   }
