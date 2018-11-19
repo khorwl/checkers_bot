34533b7bd1ef24bd4d4de200a3daeda252fb73e4
Daria Novikova <khorwl@yandex.ru>
2018-11-19 11:13:37 +0500

    SetUp functionality 'game over'

--- server/src/core/checkers/Game.java
+++ server/src/core/checkers/Game.java
@@ -3,9 +3,11 @@ package core.checkers;
 import core.checkers.players.IPlayer;
 import core.checkers.primitives.Checker;
 import core.checkers.primitives.Color;
+import core.checkers.primitives.GameProgress;
 import core.checkers.primitives.TurnStatus;
 import core.tools.CoreException;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Game implements IGame {
     private final IPlayer whitePlayer;
@@ -35,6 +37,11 @@ public class Game implements IGame {
     public TurnStatus performNextTurn() {
         var player = state.getNextTurnOrder() == Color.WHITE ? whitePlayer : blackPlayer;
 
+        if(isGameOver() != GameProgress.IN_PROGRESS){
+            state.setProgress(isGameOver());
+            return TurnStatus.NO_TURN;
+        }
+
         if (!player.haveNextTurn())
             return TurnStatus.NO_TURN;
 
@@ -59,4 +66,17 @@ public class Game implements IGame {
     public IPlayer getBlackPlayer() {
         return blackPlayer;
     }
+
+    private GameProgress isGameOver(){
+        if(loserIsPlayer(Color.BLACK))
+            return GameProgress.FINISHED_BY_WINNING_WHITE;
+        if(loserIsPlayer(Color.WHITE))
+            return GameProgress.FINISHED_BY_WINNING_BLACK;
+        return GameProgress.IN_PROGRESS;
+    }
+
+    private boolean loserIsPlayer(Color color){
+        return board.getCheckers().stream().filter(ch -> ch.getColor() == color).collect(Collectors.toList()).size() == 0;
+    }
+}
 }
